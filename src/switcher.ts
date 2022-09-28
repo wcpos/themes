5674caeb5a9a3b5f15edc710bab0b24fd5c2468a
kilbot <paul@kilbot.com.au>
2022-09-28 18:53:13 +0100

    add defaults for switcher

--- src/switcher.ts
+++ src/switcher.ts
@@ -1,8 +1,9 @@
+import defaults from 'lodash/defaults';
 import defaultTheme from './defaultTheme';
 
 interface ThemeSwitcherProps {
-	name: string;
-	mode: 'light' | 'dark';
+	name?: string;
+	mode?: 'light' | 'dark';
 	/**
 	 * {
 	 *   width: number;
@@ -11,16 +12,20 @@ interface ThemeSwitcherProps {
 	 *   fontScale: number;
 	 * }
 	 */
-	dimensions: import('react-native').ScaledSize;
+	dimensions?: import('react-native').ScaledSize;
 }
 
 /**
  * Returns a theme
- * @param name
- * @param mode
  */
-export function switcher({ name, mode, dimensions }: ThemeSwitcherProps) {
+export function switcher(options?: ThemeSwitcherProps) {
+	const opts = defaults(options, {
+		name: 'default',
+		mode: 'light',
+		dimensions: { width: 800, height: 600, scale: 2, fontScale: 1 },
+	});
+
 	// passing dimensions back through to the theme
 	// @TODO - maybe we only need a screenSize which can be over ridden by the user?
-	return { ...defaultTheme, _dimensions: dimensions };
+	return { ...defaultTheme, _dimensions: opts.dimensions };
 }
