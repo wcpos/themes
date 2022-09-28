5fa8738a99f22722af75edd0fe4cec64308d90dc
kilbot <paul@kilbot.com.au>
2022-09-28 17:17:20 +0100

    update switcher

--- src/switcher.ts
+++ src/switcher.ts
@@ -1,10 +1,26 @@
 import defaultTheme from './defaultTheme';
 
+interface ThemeSwitcherProps {
+	name: string;
+	mode: 'light' | 'dark';
+	/**
+	 * {
+	 *   width: number;
+	 *   height: number;
+	 *   scale: number;
+	 *   fontScale: number;
+	 * }
+	 */
+	dimensions: import('react-native').ScaledSize;
+}
+
 /**
  * Returns a theme
  * @param name
  * @param mode
  */
-export function switcher(name = 'default', mode = 'light') {
-	return defaultTheme;
+export function switcher({ name, mode, dimensions }: ThemeSwitcherProps) {
+	// passing dimensions back through to the theme
+	// @TODO - maybe we only need a screenSize which can be over ridden by the user?
+	return { ...defaultTheme, _dimensions: dimensions };
 }
